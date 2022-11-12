873d106c8108084c08b2d0eec90063d41d281bc8
Vladimir Boltin <boltinvlad@yandex.ru>
2022-11-12 21:55:00 +0300

    feat: make dark mode

--- js/main.js
+++ js/main.js
@@ -1,13 +1,37 @@
 function burgerMenu() {
   const menu = document.querySelector('#menu')
   const burger = document.querySelector('.burger')
+  const main = document.querySelector('#main')
 
   burger.addEventListener('click', () => {
     burger.classList.toggle('active')
 
     menu.classList.toggle('hidden')
     menu.classList.toggle('flex')
+    main.classList.toggle('mt-32')
   })
 }
 
 burgerMenu()
+
+if (
+  localStorage.theme === 'dark' ||
+  (!('theme' in localStorage) &&
+    window.matchMedia('(prefers-color-scheme: dark)').matches)
+) {
+  document.documentElement.classList.add('dark')
+} else {
+  document.documentElement.classList.remove('dark')
+}
+
+const modeBtn = document.querySelector('#mode')
+
+modeBtn.addEventListener('click', () => {
+  document.querySelector('html').classList.toggle('dark')
+
+  if (document.querySelector('html').classList.contains('dark')) {
+    localStorage.theme = 'dark'
+  } else {
+    localStorage.theme = 'light'
+  }
+})
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./*.html'],
+  darkMode: 'class',
   theme: {
     color: {
       lightblack: '#4D4244',
